f70184c18ab8701ea99286e132d8a193bcdd01c2
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2020-12-08 12:38:38 +0200

    Add Event#post()

--- src/main/java/com/griefdefender/api/event/Event.java
+++ src/main/java/com/griefdefender/api/event/Event.java
@@ -1,6 +1,13 @@
 package com.griefdefender.api.event;
 
+import com.griefdefender.api.GriefDefender;
+
 public interface Event {
 
     EventCause getCause();
+
+    default <T extends Event> T post() {
+        GriefDefender.getEventManager().post(this);
+        return (T) this;
+    }
 }
